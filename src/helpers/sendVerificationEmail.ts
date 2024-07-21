093576ca5435dfc60c65d89e456694189e660ca4
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-07-21 17:41:55 +0530

    added sign-up api with email

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,29 @@
+import { Resend } from "resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { resend } from "@/lib/resend";
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string
+):Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'AnonBox | Verification code',
+            react: VerificationEmail({username,otp:verifyCode}),
+          });
+        return {
+            success:true,
+            message:"Verification email sent successfully!"
+        }
+    } catch (error) {
+        console.error("error sending verification email ",error)
+        return {
+            success:false,
+            message:"error sending verification email"
+        }
+    }
+}
